406a8849ca702afe29568f570607003fbf6b91b5
enju <enju@protonmail.com>
2019-01-31 09:57:04 +0100

    added path variables

--- README.md
+++ README.md
@@ -1,8 +1,39 @@
 #chdl
 
-Downloading tool for 4chan threads.
+Downloader for 4chan threads.
+
+##Installing
+
+`go get github.com/enjuus/chdl`
+
+
+##Usage
+
+```
+Usage: chdl [options]
+Options:
+  -d, --destination string
+        The path to save to. See README for more options. [required]
+  -h, --help
+        Display this help message
+  -s, --source string
+        The thread to download [required]
+```
+
+`--destination` supports 3 variables:
+
+`{THREAD}`: The title of the thread. If no title is found, the thread's ID will be used.
+
+`{THREADID}`: The ID of the thread.
+
+`{BOARD}`: The board the thread is on.
+
+
+for example:
+
+`chdl -s http://boards.4chan.org/wg/thread/7289180 -d "/home/user/pictures/{BOARD}/{THREAD}"`
+
 
 ###TODO
 
-- detect if image already exists
 - create thread watcher
--- main.go
+++ main.go
@@ -1,93 +1,101 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "path"
-  "io"
-  "regexp"
-  flag "github.com/ogier/pflag"
-  "github.com/PuerkitoBio/goquery"
-  "net/http"
-  "os"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	flag "github.com/ogier/pflag"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"regexp"
+	"strings"
 )
 
 var help bool
-var name bool
 var source string
 var destination string
-var threadName string
 
 func init() {
-  flag.BoolVarP(&help, "help", "h", false, "Display this help message")
-  flag.BoolVarP(&name, "name", "n", false, "Store images in a subfolder named after the thread")
-  flag.StringVarP(&source, "source", "s", "", "The thread to download [required]")
-  flag.StringVarP(&destination, "destination", "d", "", "The path to save [required]")
-  flag.Parse()
+	flag.BoolVarP(&help, "help", "h", false, "Display this help message")
+	flag.StringVarP(&source, "source", "s", "", "The thread to download [required]")
+	flag.StringVarP(&destination, "destination", "d", "", "The path to save to. See README for more options. [required]")
+	flag.Parse()
 }
 
 func GetImages() {
-  resp, err := http.Get(source)
-  if err != nil {
-    panic(err)
-  }
+	resp, err := http.Get(source)
+	if err != nil {
+		panic(err)
+	}
 
-  defer resp.Body.Close()
+	defer resp.Body.Close()
 
-  document, err := goquery.NewDocumentFromReader(resp.Body)
-  if err != nil {
-    log.Fatal("Error loading HTTP response body.", err)
-  }
+	document, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal("Error loading HTTP response body.", err)
+	}
 
-    reg, _ := regexp.Compile("[^a-zA-Z0-9 ]+")
-    folder := document.Find(".board .subject:first-of-type").Text()
-  if name == true {
-    destination = destination + "/" + reg.ReplaceAllString(folder, "")
-  }
+	RenameFilePath(document)
+	// find images
+	document.Find(".board a.fileThumb").Each(func(index int, element *goquery.Selection) {
+		imgSrc, exists := element.Attr("href")
+		if exists {
+			fmt.Println(imgSrc)
+			StoreFile(imgSrc)
+		}
+	})
+}
 
-  // find images
-  document.Find(".board a.fileThumb").Each(func(index int, element *goquery.Selection) {
-    imgSrc, exists := element.Attr("href")
-    if exists {
-      fmt.Println(imgSrc)
-      StoreFile(imgSrc)
-    }
-  })
+func RenameFilePath(document *goquery.Document) {
+	reg, _ := regexp.Compile("[^a-zA-Z0-9 ]+")
+	parts := strings.Split(source, "/")
+	thread := reg.ReplaceAllString(document.Find(".board .subject:first-of-type").Text(), "")
+	threadId := parts[5]
+	board := parts[3]
+
+	destination = strings.Replace(destination, "{BOARD}", board, -1)
+	if thread != "" {
+		destination = strings.Replace(destination, "{THREAD}", thread, -1)
+	} else {
+		destination = strings.Replace(destination, "{THREAD}", threadId, -1)
+	}
+	destination = strings.Replace(destination, "{THREADID}", threadId, -1)
 }
 
 func StoreFile(imgSrc string) {
-  if _, err := os.Stat(destination); os.IsNotExist(err) {
-    os.MkdirAll(destination, os.ModePerm);
-  }
+	if _, err := os.Stat(destination); os.IsNotExist(err) {
+		os.MkdirAll(destination, os.ModePerm)
+	}
 
-  response, err := http.Get("https:" + imgSrc)
-  if err != nil {
-    log.Fatal(err)
-  }
+	response, err := http.Get("https:" + imgSrc)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  defer response.Body.Close()
+	defer response.Body.Close()
 
-  file, _ := os.Create(destination + "/" + path.Base(imgSrc))
-  defer file.Close()
+	file, _ := os.Create(destination + "/" + path.Base(imgSrc))
+	defer file.Close()
 
-  io.Copy(file, response.Body)
+	io.Copy(file, response.Body)
 }
 
 func main() {
-  if help == true {
-    PrintHelpMessage()
-  }
-  if source == "" && destination == "" {
-    PrintHelpMessage()
-  }
-
-  GetImages()
+	if help == true {
+		PrintHelpMessage()
+	}
+	if source == "" && destination == "" {
+		PrintHelpMessage()
+	}
+	GetImages()
 
 }
 
 func PrintHelpMessage() {
-  fmt.Printf("Usage: %s [options]\n", os.Args[0])
-  fmt.Printf("Options:\n")
-  flag.PrintDefaults()
-  os.Exit(1)
+	fmt.Printf("Usage: %s [options]\n", os.Args[0])
+	fmt.Printf("Options:\n")
+	flag.PrintDefaults()
+	os.Exit(1)
 }
